73d1ce6da75f12e83fc40da86841979056646a06
shoter <lubiebardzozelki@gmail.com>
2019-02-08 10:24:12 +0100

    Added something to display

---
+++ Chatter.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chatter.App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Something");
+            Console.ReadKey();
+        }
+    }
+}
